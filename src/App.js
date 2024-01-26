7b102e4f9415f47c8220e468144bc3c1cee23d02
nicole_s <15011784+nicolelaine@users.noreply.github.com>
2024-01-26 15:53:17 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import "./styles.css";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [theme, setTheme] = useState(
+    JSON.parse(window.localStorage.getItem("theme")) || "light"
+  );
+
+  useEffect(() => {
+    window.localStorage.setItem("theme", JSON.stringify(theme));
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <div className={`App ${theme}`} value={theme}>
+      <div className="wrapper">
+        <button onClick={toggleTheme}>
+          {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+          Toggle theme
+        </button>
+        <p>
+          <span className="accent">Fiona the flamingo loved to travel.</span>{" "}
+          She flew over mountains, deserts, and oceans, making new friends along
+          the way. When she returned home, she had amazing stories to share and
+          was ready for her next adventure. But alas, she had not learned to
+          code, so her story was nowhere near as inpiring as it could have been.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
